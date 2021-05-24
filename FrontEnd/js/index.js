1164c072aeb16c4ae4c6f355d4b16792772a30bf
Niko <nikolla@wanadoo.fr>
2021-05-24 10:19:20 +0200

    ajout input order.html

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,10 +1,14 @@
+const productTeddy = document.querySelector('#cardProduct');
+console.log(productTeddy)
+
+
 teddies()
 
 async function  teddies(){
     const articlesTeddies = await getArticlesTeddies()
     displayArticlesTeddies(articlesTeddies)
 }
-
+productTeddy.addEventListener('click', getArticlesTeddies )
 
 function getArticlesTeddies(){
     return fetch("http://localhost:3000/api/teddies")
@@ -24,14 +28,14 @@ function displayArticlesTeddies(articleTeddies){
     document.getElementById("product").innerHTML=`
     <a href="" class="stretched-link" ></a>
     <h2 class="card-header my-3 text-center" id="productName">
-    ours
+    Ours en peluche
     </h2>
     <div class="card-body">
         <img class="img-fluid imgProduct" id="productImg">
         <h4 class="card-text text-center m-5">
     
         </h4>
-        <h4 class="card-price text-end" id="productPrice"></h4>
+        <h4 class="card-price text-end" id="productPrice">${articleTeddies.price}</h4>
     
     `
 }
